81506b4698cdb84021a58af60252813df17cbb95
Suh Moraes <suh.alvesmoraes@gmail.com>
2021-09-08 09:54:47 -0300

    Learning Process with Java and Object Orientation

---
+++ codebank/src/Conta.java
@@ -0,0 +1,22 @@
+public class Conta {
+
+        double saldo;
+        int agencia;
+        int numero;
+        String titular;
+
+        // Comportamentos de uma conta
+        public void saca(){
+
+        }
+
+        public void deposita(){
+
+        }
+
+        public void transfere() {
+
+        }
+}
+
+
---
+++ codebank/src/ContaUsuario.java
@@ -0,0 +1,16 @@
+public class ContaUsuario {
+
+        public static void main(String[] args) {
+            Conta cc1 = new Conta();
+
+            cc1.saldo = 200;
+            cc1.agencia = 001;
+            cc1.numero = 12334-8;
+            cc1.titular = "Fernando";
+
+            System.out.println("Conta: " + cc1.numero);
+
+
+        }
+
+}
