e5f85e006c9cc2e8e9b2c9d8c969c63ddb2bf1ab
Suhailah de Moraes <68478385+SuhMoraes@users.noreply.github.com>
2022-03-13 15:44:09 -0300

    Test JUnit

--- 10_TDD_Junit/br/com/alura/tdd/test/java/ReajusteServiceTest.java
+++ 10_TDD_Junit/br/com/alura/tdd/test/java/ReajusteServiceTest.java
@@ -3,6 +3,7 @@ package br.com.alura.tdd.test.java;
 import br.com.alura.tdd.Desempenho;
 import br.com.alura.tdd.modelo.Funcionario;
 import br.com.alura.tdd.service.ReajusteService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
@@ -11,34 +12,30 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReajusteServiceTest {
+    private ReajusteService service;
+    private Funcionario func;
+
+    @BeforeEach
+    public void inicializar(){
+        service = new ReajusteService();
+        func = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
+    }
 
     @Test
     public void reajusteDeveriaSerDeTresPorCentoQuandoDesempenhoForADesejar(){
-        ReajusteService service = new ReajusteService();
-        Funcionario func = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
-
         service.concederReajuste(func, Desempenho.A_DESEJAR);
-
         assertEquals(new BigDecimal("1030.00"), func.getSalario());
     }
 
     @Test
     public void reajusteDeveriaSerDeQuinzePorCentoQuandoDesempenhoForBom(){
-        ReajusteService service = new ReajusteService();
-        Funcionario func = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
-
-        service.concederReajuste(func, Desempenho.BOM);
-
+         service.concederReajuste(func, Desempenho.BOM);
         assertEquals(new BigDecimal("1150.00"), func.getSalario());
     }
 
     @Test
     public void reajusteDeveriaSerDeVintePorCentoQuandoDesempenhoForOtimo(){
-        ReajusteService service = new ReajusteService();
-        Funcionario func = new Funcionario("Ana", LocalDate.now(), new BigDecimal("1000"));
-
-        service.concederReajuste(func, Desempenho.OTIMO);
-
+       service.concederReajuste(func, Desempenho.OTIMO);
         assertEquals(new BigDecimal("1200.00"), func.getSalario());
     }
 }
